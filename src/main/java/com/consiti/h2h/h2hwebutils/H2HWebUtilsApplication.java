72c1308d240a2fa50b6ba7754ab0effbda701909
carlossalazar7 <carlossalazar2228@gmail.com>
2021-09-27 16:29:06 -0600

    Initial commit

---
+++ src/main/java/com/consiti/h2h/h2hwebutils/H2HWebUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.consiti.h2h.h2hwebutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2HWebUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2HWebUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/consiti/h2h/h2hwebutils/H2HWebUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consiti.h2h.h2hwebutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2HWebUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
